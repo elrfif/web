0a499c537bf05b2f1909f333f87cf41d46b2da0b
Ahmad El Rafif <yeyerafif@gmail.com>
2026-04-24 09:22:34 +0700

    Upload github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pos ABC": {
+        judul: "sistem pos untuk PT ABC",
+        keahlian: ["javascript","PHP","HTML"],
+        gambar: "https://upload.wikimedia.org/wikipedia/commons/1/18/ABC_food_%282016%29.svg",
+        latar: "Ini adalah proyek magang  dimana saya membuat sistem POS untuk PT ABC",
+        tantangan: "Kebutuhan tentang PT ABC cukup rumit",
+        pelajaran: "Saya mendapatkan pelajaran bahwa sistem POS PT ABC ideal untuk skala kecil",
+        link: ""
+    },
+};
