d076d172b41e2366f2f9a9229e6544638de27a89
Ahmad El Rafif <yeyerafif@gmail.com>
2026-04-24 10:58:07 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const databaseProyek = {
     "sistem_pos Microsoft": {
-        judul: "sistem pos untuk Microsoft",
+        judul: "sistem pos untuk Coca Cola",
         keahlian: ["javascript","PHP","HTML"],
-        gambar: "https://blogs.microsoft.com/wp-content/uploads/prod/2012/08/8867.Microsoft_5F00_Logo_2D00_for_2D00_screen-1024x376.jpg",
-        latar: "Ini adalah proyek magang  dimana saya membuat sistem POS untuk PT ABC",
+        gambar: "https://rabbitlogo.com/wp-content/uploads/2025/09/coca-cola-logo-800x450.jpg",
+        latar: "Ini adalah proyek magang  dimana saya membuat sistem POS untuk PT Coca Cola",
         tantangan: "Tantangan yang saya hadapi meliputi manajemen waktu peningkatan kemampuan teknis, dan penyusain dengan kebutuhan proyek.",
         pelajaran: "pembelajaran yang saya dapatkan meliputi peningkatan keterampilan,manajemen waktu, serta kemampuan beradaptasi dan menyelesaikan masalah.",
         link: ""
