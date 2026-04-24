ffb37a5875121e02ece406752b046db4222ca089
Ahmad El Rafif <yeyerafif@gmail.com>
2026-04-24 11:08:47 +0700

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ const databaseProyek = {
     "sistem_pos Samsung": {
         judul: "sistem pos untuk Samsung",
         keahlian: ["javascript","PHP","HTML"],
-        gambar: "https://rabbitlogo.com/wp-content/uploads/2025/09/coca-cola-logo-800x450.jpg",
+        gambar: "https://www.qwords.com/blog/wp-content/uploads/2020/07/logo-samsung-300x168.jpg",
         latar: "Ini adalah proyek magang  dimana saya membuat sistem POS untuk PT Samsung",
         tantangan: "Tantangan yang saya hadapi meliputi manajemen waktu peningkatan kemampuan teknis, dan penyusain dengan kebutuhan proyek.",
         pelajaran: "pembelajaran yang saya dapatkan meliputi peningkatan keterampilan,manajemen waktu, serta kemampuan beradaptasi dan menyelesaikan masalah.",
