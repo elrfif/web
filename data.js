1982fbdff5f9d3631d93be9929727cf9756a4a56
Ahmad El Rafif <yeyerafif@gmail.com>
2026-04-24 10:45:48 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const databaseProyek = {
     "sistem_pos ABC": {
         judul: "sistem pos untuk PT ABC",
         keahlian: ["javascript","PHP","HTML"],
-        gambar: "https://www.tbstat.com/cdn-cgi/image/f=avif,q=50/wp/uploads/2023/10/20230810_Solana_News2-1200x675.jpg",
+        gambar: "https://blogs.microsoft.com/wp-content/uploads/prod/2012/08/8867.Microsoft_5F00_Logo_2D00_for_2D00_screen-1024x376.jpg",
         latar: "Ini adalah proyek magang  dimana saya membuat sistem POS untuk PT ABC",
         tantangan: "Kebutuhan tentang PT ABC cukup rumit",
         pelajaran: "Saya mendapatkan pelajaran bahwa sistem POS PT ABC ideal untuk skala kecil",
